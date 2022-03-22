f620b60cb1fc7933f48a71846a9109ca77647b6e
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-22 17:43:13 -0400

    finished notes.js file and routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,26 @@
-const express = require('express');
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, readAndAppend } = require("../helpers/util");
 
-const app = express();
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
-const notes = require('express').Router();
+notes.post("/", (req, res) => {
+  const { text, title } = req.body;
 
+  if (req.body) {
+    const newNote = {
+      text,
+      title,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.status(200).json(`Note added successfully`);
+  } else {
+    res.status(404).send("Error in adding note");
+  }
+});
+
+module.exports = notes;
